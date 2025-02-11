9f30760ddc3f5f1ba589776355278cf976866d09
Adrian Skawinski2 <adrianskawinski2@MacBook-Air-Adrian.local>
2025-02-11 15:52:32 +0100

    Changed logger

--- app/core/logger.py
+++ app/core/logger.py
@@ -1,13 +1,11 @@
 import logging
+from app.core.config import LOG_LEVEL
 
-# Define a consistent log format with timestamp, log level, module, and message
 LOG_FORMAT = "%(asctime)s - [%(levelname)s] - %(module)s - %(message)s"
 
-# Configure the logger with the specified format and default log level (INFO)
 logging.basicConfig(
     format=LOG_FORMAT,
-    level=logging.INFO,  # Change to DEBUG, WARNING, ERROR as needed
+    level=LOG_LEVEL,
 )
 
-# Create a logger instance for the application
 logger = logging.getLogger("geolocation_app")
